69a3deda26c801d5bf969dfc581e13edaa3e1296
Marcus Ho <marcusho1993@gmail.com>
2022-04-14 17:28:48 -0400

    updated index.js for deployment

--- server/index.js
+++ server/index.js
@@ -11,9 +11,22 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const PORT = process.env.PORT || 4000;
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 connectDB();
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+	const __dirname = path.dirname(fileURLToPath(import.meta.url));
+	app.use(express.static(path.join(__dirname, '../client/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.status(201).json({ message: 'WELCOME TO SERVER' });
+	});
+}
 
 async function startApolloServer(typeDefs, resolvers) {
 	const app = express();
@@ -29,12 +42,6 @@ async function startApolloServer(typeDefs, resolvers) {
 			return { userId };
 		},
 	});
-
-	app.use(express.static(path.join(__dirname, '../client/build')));
-
-	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-	);
 
 	await server.start();
 	server.applyMiddleware({ app });
