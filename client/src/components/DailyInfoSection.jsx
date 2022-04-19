52f34377ea665fd60f309d9937f4e33f720bdd63
Marcus Ho <marcusho1993@gmail.com>
2022-04-18 20:09:21 -0400

    test async issue on heroku

--- client/src/components/DailyInfoSection.jsx
+++ client/src/components/DailyInfoSection.jsx
@@ -1,12 +1,12 @@
-import { useQuery } from '@apollo/client';
+import { useLazyQuery } from '@apollo/client';
 import { useEffect, useState } from 'react';
-import Table from 'react-bootstrap/Table';
 import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import { FaPencilAlt } from 'react-icons/fa';
 import { DAILY_INFO_QUERY } from '../graphql/daily-info';
 import useDate from '../hooks/useDate';
 import DailyInfoModalForm from './DailyInfoModalForm';
-import { FaPencilAlt } from 'react-icons/fa';
-import Button from 'react-bootstrap/Button';
 
 function DailyInfoSection() {
 	const [dailyInfoRecords, setDailyInfoRecords] = useState([]);
@@ -15,37 +15,42 @@ function DailyInfoSection() {
 	const [isAddedDailyInfo, setIsAddedDailyInfo] = useState(false);
 	const [showModal, setShowModal] = useState(false);
 
-	useQuery(DAILY_INFO_QUERY.GET_DAILY_INFO, {
-		onCompleted: (data) => {
-			setDailyInfoRecords(
-				data.dailyInfo.map((di) => ({
-					...di,
-					date: getDateStringFromMilliseconds(di.date),
-				}))
-			);
-		},
-	});
+	const [getDailyInfoRecords, { loading }] = useLazyQuery(
+		DAILY_INFO_QUERY.GET_DAILY_INFO,
+		{
+			onCompleted: (data) => {
+				setDailyInfoRecords(
+					data.dailyInfo.map((di) => ({
+						...di,
+						date: getDateStringFromMilliseconds(di.date),
+					}))
+				);
+			},
+		}
+	);
 
 	useEffect(() => {
-		const checkIfDailyInfoExists = () => {
-			const isDailyInfoExists = dailyInfoRecords.some(
-				(di) =>
-					di.date ===
-					new Date().toLocaleDateString(undefined, { dateStyle: 'medium' })
-			);
+		getDailyInfoRecords();
+	}, [getDailyInfoRecords]);
 
-			if (isDailyInfoExists) {
-				setIsAddedDailyInfo(true);
-			}
-		};
+	useEffect(() => {
+		const isDailyInfoExists = dailyInfoRecords.some(
+			(di) =>
+				di.date ===
+				new Date().toLocaleDateString(undefined, { dateStyle: 'medium' })
+		);
 
-		checkIfDailyInfoExists();
+		if (isDailyInfoExists) {
+			setIsAddedDailyInfo(true);
+		}
 	}, [dailyInfoRecords, getDateStringFromMilliseconds]);
 
 	const onEdit = (di) => {
 		setEditDailyInfoRecord(di);
 		setShowModal(true);
 	};
+
+	if (loading) return <div>Loading...</div>;
 
 	return (
 		<>
